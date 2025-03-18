698b201b152f2f31204d076ddac050ee110691a8
Peter Nicholls <peterwnicholls@gmail.com>
2025-03-18 11:03:13 +1100

    WIP

--- Sources/UserKit/APIClient/APIClient.swift
+++ Sources/UserKit/APIClient/APIClient.swift
@@ -7,8 +7,8 @@
 
 import Foundation
 
-let baseURL = "https://turns-habitat-roller-deadly.trycloudflare.com"
-//let baseURL = "https://getuserkit.com"
+//let baseURL = "https://turns-habitat-roller-deadly.trycloudflare.com"
+let baseURL = "https://getuserkit.com"
 
 actor APIClient {
     private var accessToken: String?
